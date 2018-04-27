2f268c4c427cfe5eaf13ac466bf19ee4c49c9adb
MrB <patonacevedo@gmail.com>
2018-04-26 20:10:53 -0500

    Adding block and testing for valid chain(blocks with prevBLock hash)

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -4,6 +4,16 @@ class Blockchain {
     constructor() {
         this.chain = [Block.genesis()];
     }
+    
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(data) {
+        const newBlock = Block.mineBlock(this.getLastBlock(), data)
+        this.chain.push(newBlock);
+        return newBlock;
+    }
 }
 
 module.exports = Blockchain;
--- blockchain/blockchain.test.js
+++ blockchain/blockchain.test.js
@@ -12,4 +12,24 @@ describe('Blockchain', () => {
     it('clockchain genesis block = genesis block', () => {
         expect(blockchain.chain[0]).toEqual(Block.genesis());
     });
+
+    it('mined block verified data', () => {
+        const data = 'test';
+        const newBlock = blockchain.addBlock(data);
+        expect(newBlock.data).toEqual(data);
+    });
+
+    it('blocks in chain must have/match prevBlock hash', () => {
+
+        const data = 'test';
+        blockchain.addBlock(data);
+        blockchain.addBlock(data);
+        blockchain.addBlock(data);
+
+        for(index = 1; index <= blockchain.chain.length -1; index++) {
+            let block = blockchain.chain[index];
+            let prevBlock = blockchain.chain[index -1];
+            expect(block.prevHash).toEqual(prevBlock.hash);
+        }
+    });
 });
