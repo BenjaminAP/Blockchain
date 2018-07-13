65418eb891f25c653024530fe20abf50a570ea2c
BenjaminAP <patonacevedo@gmail.com>
2018-07-13 00:47:34 -0400

    Added two-way-binding in project

---
+++ BiriCoin/src/app/blockchain/blockchain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blockchain',
+  templateUrl: './blockchain.component.html',
+  styleUrls: ['./blockchain.component.css']
+})
+export class BlockchainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
