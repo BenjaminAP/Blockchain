ffc780945b8ac7680d8f965238878f3deb8e3289
BenjaminAP <patonacevedo@gmail.com>
2018-07-20 12:04:37 -0400

    Divided imports by components/services/directives

--- BiriCoin/src/app/app.module.ts
+++ BiriCoin/src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 
-
+//COMPONENTS------------------------------------------------------------------------------------------------------------
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { BlockchainComponent } from './components/blockchain/blockchain.component';
 import { BlockComponent } from './components/blockchain/block/block.component';
+import { BlockchainComponent } from './components/blockchain/blockchain.component';
 import { WalletComponent } from './components/wallet/wallet.component';
 import { TransactionComponent } from './components/transaction/transaction.component';
+
+//SERVICES--------------------------------------------------------------------------------------------------------------
 import { BlockchainService } from "./service/blockchain/blockchain.service";
+
+//DIRECTIVES------------------------------------------------------------------------------------------------------------
 import { TitleCasePipe } from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlockchainComponent,
     HeaderComponent,
     BlockComponent,
+    BlockchainComponent,
     WalletComponent,
     TransactionComponent
   ],
